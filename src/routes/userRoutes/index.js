08260e716c130f0516b9b946288b618617f7e11f
Lucas Gabriel Ruiz <lucaschatoruiz@gmail.com>
2025-03-18 21:13:45 -0300

    commit to update deployment

--- src/routes/userRoutes/index.js
+++ src/routes/userRoutes/index.js
@@ -9,7 +9,8 @@ router.post('/create', userController.createUser);
 router.post('/update', userController.updateUser);
 
 // // Ruta para obtener todos los usuarios
-router.get('/', authenticateJWT, userController.getAllUsers);
+// router.get('/', authenticateJWT, userController.getAllUsers);
+router.get('/', userController.getAllUsers);
 
 // Ruta para login
 router.post('/login', userController.loginUser);
